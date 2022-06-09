44f798cdb67f813e8624d113336da1ddbf8c206a
Arturo Garcia <agz_96@hotmail.com>
2022-06-08 21:52:32 -0500

    Tutorial numbers section

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 print('Hello world noch einmal, my age is', 26) 
 print("Welcome again")
 
+## Strings
+
 name = "TIM"
 age = 18
 
 print(name, "is my name and my age is", age)
 print(name + " is my name")
 
-print("Hi. \n How are you", name,"?")
+print("Hi. \n How are you", name,"?") #\n to pass space
 
 print("First letter of", name, "is",name[0])
 
@@ -19,3 +21,32 @@ print(len(name))
 
 print(name.index('I'))
 print(name.replace('M','C'))
+
+
+## Numbers
+
+print(78)
+number = 79
+print(number)
+print(78+22)
+print(78+22.934)
+print(78-22.934)
+print(78/22.934)
+print(78*22.934)
+print(20%6) ## To get the reminder of a division
+
+number = 55
+number2 = str(number)
+print(number2)
+print("number is "+number2)
+
+print(-5)
+print(abs(-5)) ## Absolute value function
+print(max(4,2,3)) ## Maximum value
+print(min(2,3,5,6))
+print(bin(3))
+print(bin(334))
+
+from math import *
+
+print(sqrt(100))
