71b151c5c94f8ebbb32c8f70a7734013285aa7ca
Arturo Garcia <agz_96@hotmail.com>
2022-06-08 21:39:57 -0500

    First part of the tutorial (strings)

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-print("Hello world noch einmal") 
+print('Hello world noch einmal, my age is', 26) 
 print("Welcome again")
+
+name = "TIM"
+age = 18
+
+print(name, "is my name and my age is", age)
+print(name + " is my name")
+
+print("Hi. \n How are you", name,"?")
+
+print("First letter of", name, "is",name[0])
+
+print(name.upper())
+print(name.lower())
+print(name.isupper())
+
+print(len(name))
+
+print(name.index('I'))
+print(name.replace('M','C'))
